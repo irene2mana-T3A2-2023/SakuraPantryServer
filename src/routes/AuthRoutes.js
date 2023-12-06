cc82925dfed48d692500afcc68b7aa8ebf409bea
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-12-06 14:31:00 +1100

    Implement api endpoint for resgistration

---
+++ src/routes/AuthRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { register } from '../controllers/AuthController.js';
+
+const router = express.Router();
+
+router
+  .post('/auth/register', register)
+
+export default router;
