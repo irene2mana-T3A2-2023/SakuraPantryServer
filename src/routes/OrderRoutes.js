d8bdc3c85668e018828365db0b08cabe5821cf03
Mana Misumi <mana.misumi3050@gmail.com>
2023-12-07 14:22:26 +1100

    fix: OrderId => orderId

--- src/routes/OrderRoutes.js
+++ src/routes/OrderRoutes.js
@@ -6,12 +6,12 @@ const router = express.Router();
 
 // Get a list of all orders
 router.get('/orders', OrderController.getAllOrders);
-// Get a specific order by OrderId
-router.get('/orders/:OrderId', OrderController.getOrderById);
+// Get a specific order by orderId
+router.get('/orders/:orderId', OrderController.getOrderById);
 // Create a new orders
 router.post('/orders', OrderController.createOrder);
-// Update order status by OrderId
-router.patch('/orders/status/:OrderId', OrderController.updateOrderStatus);
+// Update order status by orderId
+router.patch('/orders/status/:orderId', OrderController.updateOrderStatus);
 
 
 export default router;
