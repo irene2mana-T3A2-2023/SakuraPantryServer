713e3416e0cbf426c838a2e1717df7951b2a43a4
ellenpham <ellenpham1510@gmail.com>
2023-12-08 19:10:00 +1100

    fix: change logic for authMiddleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,20 +1,16 @@
-import User from '../models/UserModel.js';
+// import User from '../models/UserModel.js';
 
 // Admin Authorization Middleware
 const isAdmin = async (req, res, next) => {
   try {
-    const userId = req.user.id;
-
-    // Fetch the user from the database
-    const user = await User.findById(userId);
+    const user = req.user;
 
     // Check if the user has the 'admin' role
-    if (!user || user.role !== 'admin') {
-      return res.status(403).json({ error: 'Permission denied. Admin access required.' });
+    if (user && user.role == 'admin') {
+        next();
+    } else {
+        return res.status(403).json({ error: 'Access forbidden. Admin privileges required.'})
     }
-
-    // If the user is an admin, proceed to the next middleware or route handler
-    next();
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
