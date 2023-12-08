8b12b4f2a4623c8ebf72bc633c6bcb1b764c584a
ellenpham <ellenpham1510@gmail.com>
2023-12-08 17:59:07 +1100

    fix: code formatting

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,24 +2,24 @@ import User from '../models/UserModel.js';
 
 // Admin Authorization Middleware
 const isAdmin = async (req, res, next) => {
-    try {
-      const userId = req.user.id;
-  
-      // Fetch the user from the database
-      const user = await User.findById(userId);
-  
-      // Check if the user has the 'admin' role
-      if (!user || user.role !== 'admin') {
-        return res.status(403).json({ error: 'Permission denied. Admin access required.' });
-      }
-  
-      // If the user is an admin, proceed to the next middleware or route handler
-      next();
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const userId = req.user.id;
+
+    // Fetch the user from the database
+    const user = await User.findById(userId);
+
+    // Check if the user has the 'admin' role
+    if (!user || user.role !== 'admin') {
+      return res.status(403).json({ error: 'Permission denied. Admin access required.' });
     }
-  };
+
+    // If the user is an admin, proceed to the next middleware or route handler
+    next();
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
 export default isAdmin;
