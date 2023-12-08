454e42ef780d40f8e4c2c1e5dedc3e3205f1baaa
ellenpham <ellenpham1510@gmail.com>
2023-12-08 11:04:09 +1100

    fix: code formatting

--- src/dev-data/data/testImportData.js
+++ src/dev-data/data/testImportData.js
@@ -5,19 +5,18 @@ import Product from '../../models/ProductModel.js';
 // Function to get all products belong to a category
 // This is to test if the id reference between categories and products is working
 const readData = async () => {
+  await databaseConnect();
+  const categoryId = '657046215ed01f56e0a4e007'; // replace this with different _id in categories.json to test
 
-    await databaseConnect()
-    const categoryId = '657046215ed01f56e0a4e00a'; 
+  try {
+    const products = await Product.find({ category: categoryId }).exec();
+    // eslint-disable-next-line no-console
+    console.log('Result:', products);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('An error has occured:', err);
+  }
+  process.exit();
+};
 
-    try {
-        const products = await Product.find({ category: categoryId }).exec();
-        // eslint-disable-next-line no-console
-        console.log('Result:', products);
-    } catch(err) {
-        // eslint-disable-next-line no-console
-        console.error('An error has occured:', err);
-    }
-    process.exit()
-}
-
-readData()
+readData();
