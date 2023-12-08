4294d83a4c6d74b6993f03ae775d4f975ca3f158
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-12-08 23:22:41 +1100

    rebase main

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,9 +1,5 @@
 import express from 'express';
-<<<<<<< HEAD
 import { register, login, forgotPassword, resetPassword } from '../controllers/AuthController.js';
-=======
-import { register, login, forgotPassword } from '../controllers/AuthController.js';
->>>>>>> d5fe006f4b80f9b56fbf3c6616a16908716bc23f
 
 // Create a new router object using Express
 const router = express.Router();
@@ -14,7 +10,6 @@ router.post('/auth/register', register);
 // Route for user login - POST request to '/auth/login' will be handled by the 'login' controller
 router.post('/auth/login', login);
 
-<<<<<<< HEAD
 // Route for initiating a forgot password request - POST request to '/auth/forgot-password' will be handled by the 'forgotPassword' controller
 router.post('/auth/forgot-password', forgotPassword);
 
@@ -22,7 +17,4 @@ router.post('/auth/forgot-password', forgotPassword);
 router.post('/auth/reset-password', resetPassword);
 
 // Export the router to be used in other parts of the application
-=======
-router.post('/auth/forgot-password', forgotPassword);
->>>>>>> d5fe006f4b80f9b56fbf3c6616a16908716bc23f
 export default router;
