3c191c899b71c641de0b14bd1c939f9141079204
ellenpham <ellenpham1510@gmail.com>
2023-12-15 10:10:03 +1100

    fix: add code comments

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -11,6 +11,7 @@ import { getUserFromJwt } from '../utils/getAuthUser.js';
 // @route   GET /api/orders
 // @access  Private/Admin
 export const getAllOrders = catchAsync(async (req, res, next) => {
+  // Retrieve orders from db and populate the 'user' field with selected user properties
   const results = await Order.find({}).populate('user', 'id firstName lastName email');
   res.status(200).json({
     orders: results
@@ -21,8 +22,10 @@ export const getAllOrders = catchAsync(async (req, res, next) => {
 // @route   GET /api/orders/myorders
 // @access  Private
 export const getMyOrders = catchAsync(async (req, res, next) => {
+  // Decode user info from the JWT in the request
   const decodedUser = getUserFromJwt(req);
 
+  // Retrieve orders from the db that belong to the decoded user
   const results = await Order.find({ user: decodedUser.userId });
 
   res.status(200).json(results);
@@ -32,6 +35,7 @@ export const getMyOrders = catchAsync(async (req, res, next) => {
 // @route   GET /api/orders/:id
 // @access  Private
 export const getOrderById = catchAsync(async (req, res, next) => {
+  // Retrieve an order from the db by its ID provided in the request params
   const result = await Order.findById(req.params.id).exec();
 
   if (!result) {
@@ -47,12 +51,15 @@ export const getOrderById = catchAsync(async (req, res, next) => {
 export const createOrder = catchAsync(async (req, res, next) => {
   const { orderItems, shippingAddress, paymentMethod } = req.body;
 
+  // Check if there are no order items provided
   if (orderItems && orderItems.length === 0) {
     return next(new AppError('No order items', 404));
   }
 
+  // Retrieve the user associated with the request
   const user = await User.findById(req.user.userId).exec();
 
+  // Map over order items to get details with prices
   const orderItemsWithPrices = await Promise.all(
     orderItems.map(async (item) => {
       const product = await Product.findById(item.product);
@@ -67,9 +74,11 @@ export const createOrder = catchAsync(async (req, res, next) => {
     })
   );
 
+  // Calculate the total price for the order
   let totalPrice = orderItemsWithPrices.reduce((acc, item) => acc + item.totalPrice, 0);
   totalPrice = (Math.round(totalPrice * 100) / 100).toFixed(2);
 
+  // Create a new order in the db
   const newOrder = await Order.create({
     orderItems: orderItemsWithPrices,
     user,
@@ -85,6 +94,7 @@ export const createOrder = catchAsync(async (req, res, next) => {
 // @route   GET /api/orders/:id/status
 // @access  Private/Admin
 export const updateOrderStatus = catchAsync(async (req, res, next) => {
+  // Update an order's status in the db by its ID, with the provided request body
   let result = await Order.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true
--- src/utils/getAuthUser.js
+++ src/utils/getAuthUser.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import { envConfig } from '../configs/env.js';
 import AppError from '../middlewares/appError.js';
 
+// Function to get user information from JWT
 export const getUserFromJwt = (req) => {
   // Check if JWT exists in the "Authorization" header and extract it
   const authHeader = req.header('Authorization');
@@ -9,15 +10,17 @@ export const getUserFromJwt = (req) => {
 
   if (!token) {
     // If no token, send a 401 Unauthorized response
-    throw new AppError('Token not provided. Please log in.', 400);
+    throw new AppError('Token not provided. Please log in.', 401);
   }
 
   try {
-    // Verify the JWT token
+    // Verify the JWT token using the jwtSecret from envConfig
     const decodedToken = jwt.verify(token, envConfig.jwtSecret);
 
+    // Return the decoded user information from the JWT
     return decodedToken;
   } catch (jwtError) {
-    throw new AppError('Access forbidden. Invalid token', 400);
+    // If the token verification fails, send a 401 Unauthorized response.
+    throw new AppError('Access forbidden. Invalid token', 401);
   }
 };
