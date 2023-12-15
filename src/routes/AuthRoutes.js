0140e51e1c5a4aeb552a071f8577451519871363
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-12-15 14:20:40 +1100

    fix: add admin to authoriseRole on current-user route

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -24,7 +24,7 @@ router.post('/auth/forgot-password', forgotPassword);
 router.post('/auth/reset-password', resetPassword);
 
 //Route for checking user's authentication status and allows access only to users who have the specificed role as user.
-router.get('/auth/current-user', isAuthenticatedUser, authoriseRole(['user']), currentUser);
+router.get('/auth/current-user', isAuthenticatedUser, authoriseRole(['user', 'admin']), currentUser);
 
 // Export the router to be used in other parts of the application
 export default router;
