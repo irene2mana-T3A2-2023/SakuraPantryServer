5d4811ab887a09f19e5c2967e604ec6d3b6b2bd4
Mana Misumi <mana.misumi3050@gmail.com>
2023-12-19 11:25:50 +1100

    add comment

--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 // Get all products
 router.get('/products', ProductController.getAllProducts);
 
+// Get relative products
 router.get(
   '/products/relative-products/:categorySlug',
   ProductController.relativeProductsByCategory
