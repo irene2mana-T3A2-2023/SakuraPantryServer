03f34c4336e3f3dbab419844748dcedeee3b68f9
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-12-20 11:19:59 +1100

    add comments on the code

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,9 +3,11 @@ import catchAsync from '../utils/catchAsync.js';
 
 // @desc    Get all users
 // @route   GET /api/users
-// @access  Public
+// @access  Private/Admin
 // eslint-disable-next-line no-unused-vars
 export const getAllUsers = catchAsync(async (req, res, next) => {
+  // Fetches all users from the User model using the `find` method.
+  // Sorts the users in descending order by their creation date using the `sort` method.
   let results = await User.find({}).sort({ createdAt: -1 });
 
   res.status(200).json(results);
