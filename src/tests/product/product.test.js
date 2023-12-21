410f2e2c9929ae910e2fd783e0cee12c9d72adce
Mana Misumi <126526974+Mana12011207@users.noreply.github.com>
2023-12-21 22:22:17 +1100

    Additional unit tests for products API (#40)
    * add comments
    
    * fix: missing brackets and code formatting
    
    ---------
    
    Co-authored-by: Ellen My Pham <126633021+ellenpham@users.noreply.github.com>
    Co-authored-by: ellenpham <ellenpham1510@gmail.com>

--- src/tests/product/product.test.js
+++ src/tests/product/product.test.js
@@ -4,6 +4,9 @@ import Category from '../../models/CategoryModel.js';
 import Product from '../../models/ProductModel.js';
 
 const relativeProductsEndpoint = '/api/products/relative-products';
+const newArrivalProductsEndpoint = '/api/products/new-arrivals';
+const featuredProductsEndpoint = '/api/products/feature';
+const searchProductsEndpoint = '/api/products/search';
 const getAllProductsEndpoint = '/api/products';
 const getProductBySlugEndpoint = '/api/products';
 const createProductEndpoint = '/api/products';
@@ -26,6 +29,76 @@ describe('Products API', () => {
 
       expect(res.statusCode).toEqual(200);
       expect(res.body.length).toEqual(5);
+    });
+  });
+
+  // Test cases for getNewArrivalProducts route
+  describe(`[GET] ${newArrivalProductsEndpoint}`, () => {
+    it('Should return the latest 5 products whenre there are at least 5 new arrivals', async () => {
+      const res = await request(app).get(`${newArrivalProductsEndpoint}/`);
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.length).toEqual(5);
+    });
+  });
+
+  // Test cases for getFeatureProducts route
+  describe(`[GET] ${featuredProductsEndpoint}`, () => {
+    it('Should return 5 featured products when there are at least 5 featured products', async () => {
+      const res = await request(app).get(`${featuredProductsEndpoint}/`);
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.length).toEqual(5);
+    });
+  });
+
+  // Test cases for searchProduct route
+  describe(`[GET] ${searchProductsEndpoint}`, () => {
+    it('Should return paginated search results for products with names or descriptions containing with keyword m', async () => {
+      const keyword = 'm';
+      const page = 1;
+      const limit = 8;
+
+      const res = await request(app).get(
+        `${searchProductsEndpoint}/?k=${keyword}&c=&page=${page}&limit=${limit}`
+      );
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty('totalResults');
+      expect(res.body).toHaveProperty('totalPages');
+      expect(res.body).toHaveProperty('currentPage', page);
+      expect(res.body.results).toHaveLength(limit);
+    });
+
+    it('Should return an empty array when an invalid keyword or category slug is provided', async () => {
+      const invalidKeyword = 'nonExistentKeyword';
+      const invalidCategorySlug = 'nonExistentSlug';
+      const page = 1;
+      const limit = 8;
+
+      const res = await request(app).get(
+        `${searchProductsEndpoint}/?k=${invalidKeyword}&c=${invalidCategorySlug}&page=${page}&limit=${limit}`
+      );
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.results).toHaveLength(0);
+    });
+
+    it('Should filter products by the specified category', async () => {
+      const validCategorySlug = 'sauces-seasonings';
+      const page = 1;
+      const limit = 8;
+
+      const res = await request(app).get(
+        `${searchProductsEndpoint}/?c=${validCategorySlug}&page=${page}&limit=${limit}`
+      );
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.results).not.toHaveLength(0);
+
+      res.body.results.forEach((product) => {
+        expect(product.category.slug).toEqual(validCategorySlug);
+      });
     });
   });
 
